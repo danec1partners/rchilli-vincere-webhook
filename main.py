0e5b4adde63cc46a6a29430cf0aee4e998130b64
danec1partners <danec1partners@gmail.com>
2025-08-07 10:48:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -44,3 +44,8 @@ def webhook():
     except Exception as e:
         print("❌ Error processing webhook:", str(e))
         return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
